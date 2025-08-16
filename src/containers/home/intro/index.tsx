5ae89f6bbc910b721237830c5f7f6012edd16f38
Vincent Samaco <vsamaco@gmail.com>
2025-08-15 17:02:18 -0700

    setup layout and containers

---
+++ src/containers/home/intro/index.tsx
@@ -0,0 +1,5 @@
+const IntroContainer = () => {
+  return <div className="intro-slider-wrap">INntroContainer</div>;
+};
+
+export default IntroContainer;
---
+++ src/layouts/index.tsx
@@ -0,0 +1,5 @@
+const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default Layout;
