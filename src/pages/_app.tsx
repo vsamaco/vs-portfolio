d35b52609f50da0e9f816be61c8f263080d6310a
Vincent Samaco <vsamaco@gmail.com>
2025-08-20 15:46:41 -0700

    import header styles and fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "@/styles/scss/style.scss";
+import "@/styles/css/elegantIcons.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
