2c7d7e55f53f52bcbe727a67d8ba767bac83d486
Vincent Samaco <vsamaco@gmail.com>
2025-08-21 22:58:39 -0700

    migrate intro swiper, data, styles

---
+++ src/components/intro/index.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { FC } from "react";
+
+interface IntroProps {
+  data: {
+    id: number;
+    backgroundImage: string;
+    title: string;
+    subTitle: string;
+    desc: string;
+    buttonText: string;
+  };
+}
+
+const Intro: FC<IntroProps> = ({ data }) => {
+  return (
+    <div
+      className="intro-section section overlay"
+      style={{
+        background: `url(${data.backgroundImage}) no-repeat center`,
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="container">
+        <div className="row row-cols-lg-1 row-cols-1">
+          <div className="col align-self-center">
+            {/* <div className="intro-image">
+              <img src={data.backgroundImage} />
+            </div> */}
+            <div className="intro-content">
+              {/* <span className="sub-title">{data.subTitle}</span> */}
+              <h2 className="title">{data.title}</h2>
+              {/* <div className="desc">
+                <p>{data.desc}</p>
+              </div> */}
+              {/* <Link href={"/"} className="intro-btn">
+                {data.buttonText}
+              </Link> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
--- src/containers/home/intro/index.tsx
+++ src/containers/home/intro/index.tsx
@@ -1,5 +1,38 @@
+import React from "react";
+import { Pagination } from "swiper/modules";
+import "swiper/scss/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import Intro from "@/components/intro/index";
+import HomeData from "@/data/home.json";
+
 const IntroContainer = () => {
-  return <div className="intro-slider-wrap">INntroContainer</div>;
+  const swiperOption = {
+    modules: [Pagination],
+    loop: true,
+    speed: 750,
+    spaceBetween: 0,
+    slidesPerView: 1,
+    pagination: { clickable: true },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  };
+  return (
+    <div className="intro-slider-wrap">
+      <Swiper effect="fade" className="intro-slider" {...swiperOption}>
+        {HomeData[0].slider &&
+          HomeData[0].slider.map((single, key) => {
+            return (
+              <SwiperSlide key={key}>
+                <Intro data={single} key={key} />
+              </SwiperSlide>
+            );
+          })}
+      </Swiper>
+    </div>
+  );
 };
 
 export default IntroContainer;
