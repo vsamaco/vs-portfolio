4b3680e1060f0989688954fd8fb7695d1feb966b
Vincent Samaco <vsamaco@gmail.com>
2025-08-22 19:17:25 -0700

    remove css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import "@/styles/css/elegantIcons.css";
 import "@/styles/css/icofont.css";
 import "swiper/css";
-import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "@/styles/scss/style.scss";
 import type { AppProps } from "next/app";
